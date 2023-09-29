ab63d36c272dbced19342fe3cbbb2e77ea691977
rajesdrg <rajesh@hugtak.com>
2023-09-29 11:31:11 -0400

    07-numeros y funciones

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,15 @@
+import math
+
+print(round(1.3))
+print(round(1.7))
+print(round(1.5))
+print(abs(-77))
+print(abs(55))
+
+print(math.ceil(1.1)) #redondea el valor para arriba
+print(math.floor(1.9999))# redondea el valor para abajo
+print(math.isnan(23)) # nos indica que es un valor numerico
+#print(math.isinf(40/0))  #nos dice si ese numero esta en infinito o no
+#print(math.isnan("23"))# nos indica que no permite string y el valor no es numerico
+print(math.pow(10,3))
+print(math.sqrt(9))
