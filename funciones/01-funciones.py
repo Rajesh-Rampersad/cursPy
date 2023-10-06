c6b290d25a863ab0d671c66a7b989a038dba343e
rajesdrg <rajesh@hugtak.com>
2023-10-06 10:02:25 -0400

    depuraciones

---
+++ funciones/01-funciones.py
@@ -0,0 +1,10 @@
+def hola(nombre, apellido="Feliz"):
+    print("Hola mundo!")
+    print(f"Bienvenido {nombre} {apellido}")
+    
+hola("Rajesh", "Rampersad")
+hola("estoy")
+
+
+
+hola(apellido="Rampersad", nombre="Daniel")
---
+++ funciones/05-alcance.py
@@ -0,0 +1,15 @@
+
+
+
+def saludar():
+    global saludo
+    saludo = "Hola Mundo"
+    print(saludo)
+    
+def saludaChanchito():
+    saludo = "Hola Chanchito"
+    print(saludo)
+    
+
+saludar()
+saludaChanchito()
