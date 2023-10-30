b3f70aeec962dc697f22394a3436f68662b3b6b4
Rajesh-Rampersad <rajeshgarcia52@gmail.com>
2023-10-30 17:25:04 -0300

    clases

---
+++ clases/01-introducciones.py
@@ -0,0 +1,10 @@
+mensaje = "este un mensaje de tipo STR"
+print (type(mensaje))
+
+# Clase: es el plano de construccion
+# Objeto: una instancia de una Clase
+
+# Clases: es el plano de construccion de una casa
+# Objeto: la casa construida
+# Clase : Humano
+# Objeto: Nicolas, Felipe, Maria, Juan...
