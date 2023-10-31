97e191d0b002fc755fd6769cb54415d38ae9da04
Rajesh-Rampersad <rajeshgarcia52@gmail.com>
2023-10-31 13:58:27 -0300

    comparacion

---
+++ clases/08-metodos-magico.py
@@ -0,0 +1,19 @@
+class Perro:
+    
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        
+    def __del__ (self):
+        print(f"Chao perro 🥺 {self.nombre}")
+        
+    def habla(self):
+        print(f"{self.nombre} dice: Gua!")
+        
+    def __str__(self) -> str:
+        return f"Clase Perro: {self.nombre}"
+        
+
+perro = Perro("Chanchito",1)
+
+del perro
