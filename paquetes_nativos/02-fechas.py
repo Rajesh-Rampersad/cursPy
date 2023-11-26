bd8ecc9642d8233a9ff5ff3ae920a07b761dac93
Rajesh-Rampersad <danigarci.negocio@gmail.com>
2023-11-26 19:57:44 -0300

    paquetes nativos

--- paquetes_nativos/02-fechas.py
+++ paquetes_nativos/02-fechas.py
@@ -8,5 +8,5 @@ fecha = datetime(2023, 1, 1)
 fecha2 = datetime(2023, 2, 1)
 
 ahora = datetime.now()
-fechaStr = datetime.strptime("2023-01-10", "%Y-%m-%d")
+fechaStr = datetime.strptime("2023-01-.negocio@10", "%Y-%m-%d")
 print(fecha.strftime("%Y-%m-%d"))
